2788dfa484de2b3b9166d3b267bccb4ba1d52d3a
Pradeep Kumar Reddy <tpradeep247@gmail.com>
2025-05-27 00:38:24 +0530

    message

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+PERSON1=$1 
+PERSON2=$2
+
+echo "$PERSON1: hello how are tou $PERSON2"
+echo "$PERSON2: hello $PERSON1 how are you"
+echo "$PERSON1: how old are you"
+echo "$PERSON2: iam 19 yesrs old"
+echo "$PERSON1: which colour dress will you take for pongal"
+echo "$PERSON2: i will take blue colour"
+
