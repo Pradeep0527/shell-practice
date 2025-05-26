dc6258cdd9c466d59b0e0fb48a0f49cca23ff4f1
Pradeep Kumar Reddy <tpradeep247@gmail.com>
2025-05-27 01:18:11 +0530

    message

--- 07-array.sh
+++ 07-array.sh
@@ -3,3 +3,6 @@
 MOVIES=("Court" "Hit3" "pushpa2" "varsham")
 
 echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Thrid Movie: ${MOVIES[2]}"
+echo "Fourth Movie: ${MOVIES[3]}"
