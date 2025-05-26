0bb7668df1487aa71738a2070df4bd45d7ea3570
Pradeep Kumar Reddy <tpradeep247@gmail.com>
2025-05-26 23:16:49 +0530

    message

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,10 +3,10 @@
 PERSON1=Pradeep 
 PERSON2=Sai
 
-echo "&PERSON1: hello how are tou &PERSON2"
-echo "&PERSON2: hello &PERSON1 how are you"
-echo "&PERSON1: how old are you"
-echo "&PERSON2: iam 19 yesrs old"
-echo "&PERSON1: which colour dress will you take for pongal"
-echo "&PERSON2: i will take blue colour"
+echo "$PERSON1: hello how are tou $PERSON2"
+echo "$PERSON2: hello $PERSON1 how are you"
+echo "$PERSON1: how old are you"
+echo "$PERSON2: iam 19 yesrs old"
+echo "$PERSON1: which colour dress will you take for pongal"
+echo "$PERSON2: i will take blue colour"
 
